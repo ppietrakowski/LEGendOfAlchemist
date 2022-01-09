580d978c3eb1bb05dff5d03eaa3026c1856c50e7
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-09 10:22:46 +0100

    Added play button

--- src/game/MainMenu.ts
+++ src/game/MainMenu.ts
@@ -1,26 +1,32 @@
 
-import Phaser from "phaser";
+import Phaser, { DOM } from "phaser";
 export default class MainMenu extends Phaser.Scene {
+    private buttons: Array<Phaser.GameObjects.Sprite>;
+    
     constructor() { 
-        super('MainScene');
+        super('MainMenu');
+        this.buttons = [];
     }
     
     preload(): void {
-        document.getElementById('mainGame').style.visibility = 'visible';
-        document.getElementById('credits').style.visibility = 'visible';
-
-
-        document.getElementById('mainGame').onclick = this.onMainGameClicked;
-        document.getElementById('credits').onclick = this.onCreditsClicked;
+        this.load.image('play', 'assets/playBtn.png');
+        this.load.image('credits', 'assets/creditsBtn.png');
     }
 
     create(): void {
-        
+        this.buttons.push(this.add.sprite(95 + 120, 95 + 120, 'play'));
+        this.buttons[0].setInteractive({pixelPerfect: true});
+        this.buttons[0].on(Phaser.Input.Events.POINTER_OVER, () => this.buttons[0].setTint(0x787878));
+        this.buttons[0].on(Phaser.Input.Events.POINTER_OUT, () => this.buttons[0].clearTint())
+        this.buttons[0].on(Phaser.Input.Events.POINTER_UP, this.onMainGameClicked);
+
+
+        //this.buttons.push(this.add.sprite(95, 95, 'credits'));
     }
 
 
     onMainGameClicked(): void {
-
+        
     }
 
     onCreditsClicked(): void {
--- src/main.js
+++ src/main.js
@@ -30,12 +30,10 @@ class Game extends Phaser.Game {
         });
 
         this.scene.add("BootScene", BootScene, true);
-        this.scene.add("GameScene", GameScene, false);
         this.scene.add('MainMenu', MainMenu, false);
     }
 }
 
 window.onload = () => {
-    // Obiekt gry jest udostępniony przez własność `window.game`. Można z niej korzystać w dowolnym miejscu kodu.
     Object.defineProperty(window, 'game', new Game());
 }
