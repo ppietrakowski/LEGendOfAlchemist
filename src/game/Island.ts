8b10f0f12dfe8353172ef9a7ef715fad24db70ff
ppietrakowski <pawelpietrakowski22012001@gmail.com>
2022-01-11 13:19:43 +0100

    Added background of island

--- src/game/Island.ts
+++ src/game/Island.ts
@@ -1,17 +1,20 @@
 import Location from "./Location";
 
 class Island extends Location {
+    key: string;
 
     constructor(islandName: string, spawnPoint: Phaser.Math.Vector2) {
         super({key: islandName}, spawnPoint.x, spawnPoint.y);
+        this.key = islandName;
     }
 
     preload(): void {
-        
+        this.load.image(this.key, 'assets/' + this.key + '.png');
     }
     start(): void {
-        
+        this.add.image(0, 0, this.key).setOrigin(0, 0);
     }
+    
     update(time: number, delta: number): void {
        
     }
