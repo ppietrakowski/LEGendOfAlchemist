847352937797ede6981159fe92f42934fc899e18
ppietrakowski <pawelpietrakowski22012001@gmail.com>
2022-01-11 12:56:46 +0100

    Added player and gamescene

---
+++ src/game/Player.ts
@@ -0,0 +1,16 @@
+import Character from "./Character";
+
+export default class Player extends Character {
+    
+
+    start(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    makeDead(): void {
+        throw new Error("Method not implemented.");
+    }
+
+
+
+}
