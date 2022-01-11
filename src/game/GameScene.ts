b00f01f2f871dcc95decd3d071ab1d4f16a570b8
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-11 23:45:09 +0100

    Added movement and did some refactoring

--- src/game/GameScene.ts
+++ src/game/GameScene.ts
@@ -1,8 +1,6 @@
 
 import Player from './Player'
-import DeadScene from './DeadScene'
-import Attribute from './Attribute'
-import Effect from './Effect';
+import Phaser from 'phaser';
 
 export default class GameScene extends Phaser.Scene {
 
@@ -12,12 +10,10 @@ export default class GameScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.spritesheet('characters', 'assets/characters-2.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('characters', 'assets/characters-2.png', { frameWidth: 16, frameHeight: 16, spacing: 2 });
     }
 
     create(): void {
-        
-
         this.player = new Player(this.physics.add.sprite(100, 100, 'characters'));
         this.createPlayerAnims();
 
@@ -29,7 +25,7 @@ export default class GameScene extends Phaser.Scene {
     update(time: number, delta: number): void {
         this.player.update(delta / 1000);
 
-        
+
         if (this.player.isDead()) {
             this.player.makeDead();
             this.game.scene.switch('GameScene', 'DeadScene');
@@ -41,8 +37,7 @@ export default class GameScene extends Phaser.Scene {
         this.anims.create(
             {
                 key: 'front',
-                frames: anims.generateFrameNumbers('characters', { start: 1, end: 1 })
-                ,
+                frames: anims.generateFrameNumbers('characters', { start: 1, end: 1 }),
                 repeat: -1,
                 frameRate: 5
             });
@@ -53,5 +48,40 @@ export default class GameScene extends Phaser.Scene {
                 frames: anims.generateFrameNumbers('characters', { frames: [0, 2] }),
                 frameRate: 5
             });
+
+
+        this.anims.create(
+            {
+                key: 'back',
+                frames: anims.generateFrameNumbers('characters', { start: 4, end: 4 }),
+                repeat: -1,
+                frameRate: 5
+            });
+
+        this.anims.create(
+            {
+                key: 'back-run',
+                frames: anims.generateFrameNumbers('characters', { frames: [3, 5] }),
+                repeat: -1,
+                frameRate: 5
+            });
+
+        this.anims.create(
+            {
+                key: 'left-run',
+                frames: anims.generateFrameNumbers('characters', { start: 6, end: 7 }),
+                repeat: -1,
+                frameRate: 5
+            });
+
+        this.anims.create(
+            {
+                key: 'right-run',
+                frames: anims.generateFrameNumbers('characters', { start: 8, end: 9 }),
+                repeat: -1,
+                frameRate: 5
+            });
+
+
     }
 }
--- src/game/PlayerMovement.ts
+++ src/game/PlayerMovement.ts
@@ -1,9 +1,12 @@
 import Character from "./Character";
 import Component from "./Component";
 
+
 export default class PlayerMovement implements Component {
     input: Phaser.Types.Input.Keyboard.CursorKeys;
     character: Character;
+    prevMovement: string;
+
 
     debugName(): string {
         throw new Error("Method not implemented.");
@@ -12,21 +15,32 @@ export default class PlayerMovement implements Component {
         return 'movement';
     }
     start(character: Character): void {
+        this.prevMovement = 'front';
         this.character = character;
         this.input = character.sprite.scene.input.keyboard.createCursorKeys();
     }
+
     update(timeSinceLastFrame: number): void {
-        
+        timeSinceLastFrame *= 50;
+
         if (this.input.down.isDown) {
-            this.character.sprite.anims.play('front-run', true);
-            this.character.sprite.setVelocityY(100);
-        } else {
-            this.character.sprite.anims.play('front');
-            this.character.sprite.setVelocityY(0);
-        }
-        
-        // Do movement !
+            this.onMovement('front-run', 0, 100 * timeSinceLastFrame);
+            this.prevMovement = 'front';
+        } else if (this.input.up.isDown) {
+            this.onMovement('back-run', 0, -100 * timeSinceLastFrame);
+            this.prevMovement = 'back';
+        } else if (this.input.left.isDown)
+            this.onMovement('left-run', -100 * timeSinceLastFrame, 0);
+        else if (this.input.right.isDown)
+            this.onMovement('right-run', 100 * timeSinceLastFrame, 0);
+        else 
+            this.onMovement(this.prevMovement, 0, 0);
     }
 
-
+    private onMovement(frameName: string, velX: number, velY: number) {
+        
+        
+        this.character.sprite.anims.play(frameName, true);
+        this.character.sprite.setVelocity(velX, velY);
+    }
 }
