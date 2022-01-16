aad2e6cc60150e3c3b28aecb9c65fb571f4a0a58
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-16 22:26:52 +0100

    Added tileset loading into GameScene

--- src/game/Scenes/GameScene.ts
+++ src/game/Scenes/GameScene.ts
@@ -3,28 +3,35 @@ import Phaser from 'phaser';
 import Player from '../Entities/Player';
 import Enemy from '../Entities/Enemy';
 
+
+
 export default class GameScene extends Phaser.Scene {
 
     player: Player;
     testEnemy: Enemy;
+    map: Phaser.Tilemaps.Tilemap;
+    tileset: Phaser.Tilemaps.Tileset;
+    islands: Phaser.Tilemaps.TilemapLayer;
 
     constructor() {
         super('GameScene');
     }
 
     preload(): void {
-        this.load.image('background', 'assets/temp/background.png');
-        this.load.image('islands', 'assets/temp/islands.png');
+        this.load.image('main-island', 'assets/tilemap/placeholder.png');
         this.load.spritesheet('shark', 'assets/temp/shark_walk.png', {frameWidth: 32, frameHeight: 32});
         this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 64 });
+        this.load.tilemapTiledJSON('island', 'assets/tilemap/main-island.json');
     }
 
     create(): void {
-        let islands = this.physics.add.staticGroup();
-        islands.create(0, 0, 'background');
-        this.add.image(0, 0, 'islands');   
-        this.player = new Player(this.physics.add.sprite(140, 100, 'player'));
-        this.testEnemy = new Enemy('shark', 200, this.physics.add.sprite(0, 0, 'shark'), this.player);
+        this.map = this.make.tilemap({key: 'island'});
+        this.tileset = this.map.addTilesetImage('textures', 'main-island');
+        this.map.createLayer('sea', this.tileset, -100, -100);
+        this.islands = this.map.createLayer('island', this.tileset, -100, -100);
+
+        this.player = new Player(this.physics.add.sprite(220, 140, 'player'));
+        this.testEnemy = new Enemy('shark', 200, this.physics.add.sprite(230, 400, 'shark'), this.player);
     }
 
     update(time: number, delta: number): void {
