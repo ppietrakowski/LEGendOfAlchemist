a66f8e9edadbd85e2400401692c31a0b528c2230
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-19 19:13:52 +0100

    Added fields to InventoryUi class

--- src/game/Components/InventoryUi.ts
+++ src/game/Components/InventoryUi.ts
@@ -1,5 +1,35 @@
 import Phaser from 'phaser'
 
-export default class InventoryUi {
+import Character from '../Entities/Character'
+
+import Component from './Component'
+import Inventory from './Inventory'
+
+export default class InventoryUi implements Component {
+    inventory: Inventory;
+    background: Phaser.GameObjects.Sprite;
+
+    constructor(inventory: Inventory) {
+        this.inventory = inventory;
+    }
+
+    debugName(): string {
+        return " " + this;
+    }
+
+    getName(): string {
+        return 'inventory-UI';
+    }
+
+    start(character: Character): void {
+            
+    }
+
+    update(timeSinceLastFrame: number): void {
+        
+    }
+
+
+
 
 }
