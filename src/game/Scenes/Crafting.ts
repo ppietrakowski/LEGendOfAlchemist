d13b9811a5cd01dc2cee87dd531f13fd338ba102
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-28 18:12:16 +0100

    Added boilerplate code

---
+++ src/game/Scenes/Crafting.ts
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+export default class Crafting extends Phaser.Scene {
+    constructor() {
+        super('Crafting-scene');
+    }
+
+    preload(): void {
+
+    }
+
+    create(): void {
+
+    }
+
+    update(time: number, delta: number): void {
+        
+    }
+}
