307a5866c1012618e6c8013a58c0915fb8ffa7a8
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-28 22:56:19 +0100

    Now can item be dragged to fields

--- src/game/Scenes/Crafting.ts
+++ src/game/Scenes/Crafting.ts
@@ -44,10 +44,21 @@ export default class Crafting extends Phaser.Scene {
 
     create(): void {
         for (let i = 0; i < MaxItemsForCraft; i++) {
-            let field = new Field(null, this.add.image(120 + i * 24, 120, 'item-background'));
+            let field = new Field(null, this.add.image(500 + i * 48, 120, 'item-background').setOrigin(0, 0));
+            field.backgroundImage.setInteractive({dropZone: true, pixelPerfect: true});
+            field.backgroundImage.scaleX = 2;
+            field.backgroundImage.scaleY = 2;
             field.backgroundImage.name = "item-bkg-" + i;
+            field.backgroundImage.setScrollFactor(0);
+            
             this.items.push(field)
         }
+
+        this.input.on(Phaser.Input.Events.DRAG, (pointer: Phaser.Input.Pointer, gameobject: Phaser.GameObjects.Sprite, dragX: number, dragY: number) => {
+            gameobject.x = dragX;
+            gameobject.y = dragY;
+        });
+
         this.craft = new Button(this, 430, 430, 'craft-item', () => { this.onCraft() });
     }
 
@@ -59,22 +70,16 @@ export default class Crafting extends Phaser.Scene {
         sprite.name = item.sprite.name;
 
         sprite.setInteractive({pixelPerfect: true, draggable: true});
-        this.input.on(Phaser.Input.Events.DRAG, (pointer: Phaser.Input.Pointer, gameobject: Phaser.GameObjects.Sprite, dragX: number, dragY: number) => {
-            gameobject.x = dragX;
-            gameobject.y = dragY;
-        });
-
-        this.input.on(Phaser.Input.Events.DRAG, (pointer: Phaser.Input.Pointer, gameobject: Phaser.GameObjects.Sprite, dragX: number, dragY: number) => {
-            gameobject.x = dragX;
-            gameobject.y = dragY;
-        });
-
-        this.input.on(Phaser.Input.Events.DRAG_END, (pointer, gameObject, dropped) => {
-            if (!dropped) {
-                gameObject.x = gameObject.input.dragStartX;
-                gameObject.y = gameObject.input.dragStartY;
+        
+        sprite.on(Phaser.Input.Events.DROP, (pointer: Phaser.Input.Pointer, dropZone: Phaser.GameObjects.Image) => {
+            console.log(dropZone.name.startsWith('item-bkg-'));
+            if (dropZone.name.startsWith('item-bkg-')) {
+                var i = dropZone.getTopLeft()
+                sprite.setPosition(i.x + sprite.width / 2, i.y);
+                this.items.find((v) => v.backgroundImage.name === dropZone.name).item = item;
+                console.log(this.items);
             }
-        });
+        })
 
         this.container.add(sprite);
         sprite.setScrollFactor(0);
@@ -119,10 +124,17 @@ export default class Crafting extends Phaser.Scene {
         return null;
     }
 
+
     onCraft(): void {
 
         // for now just get back to gamescene
-        this.game.scene.pause('Crafting');
+        this.game.scene.pause('Crafting', () => {
+            // disable all items
+            for (let item of this.items)
+                item.item = null;
+            this.updatePosition();
+        });
+
         this.scene.setVisible(false);
         this.game.scene.run('GameScene');
     }
