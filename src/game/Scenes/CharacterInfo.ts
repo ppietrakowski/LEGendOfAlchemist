b318b7fd3fc7c984f267bcc57481f8fb0ddef623
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-29 19:08:57 +0100

    Added CharacterInfo class

---
+++ src/game/Scenes/CharacterInfo.ts
@@ -0,0 +1,12 @@
+import { Scene } from "phaser";
+
+export default class CharacterInfo extends Scene {
+    constructor() {
+        super('Character info');
+    }
+
+    preload(): void {
+
+    }
+
+}
