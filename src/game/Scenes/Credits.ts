53d8cf41495a3202f38195ed6876fa831f48bd8b
pawelp <pawelpietrakowski22012001@gmail.com>
2022-01-30 20:01:03 +0100

    added info to credits

--- src/game/Scenes/Credits.ts
+++ src/game/Scenes/Credits.ts
@@ -22,15 +22,23 @@ export default class Credits extends Phaser.Scene {
         Font: https://www.dafont.com/pixellari.font
         Sound icon: https://www.flaticon.com/premium-icon/musical-note_461146
         Background: https://pixabay.com/illustrations/planet-sience-fiction-fantasy-world-1702788/
-        
-        
-        
+
+        ## special thanks to
+        * Authors of program Tiled.
+        * Authors of program LibreSprite.
+        * Make a field Music for sounds roam1
+        * Mike Koenig for sounds: Pitch baseball, Light bulb breaking on license Creative Commons
+        * Public domain: player-slap
+        * Zacchary Dempsey-Plante for Pixellari font
+        * MoppySound for 8-bit music
+        * michaldrozdz2000 - cherries, orange, potion, red flower, player
+        * MoppySound - roam2,menu music and attack        
         `;
     }
 
     create(): void {
         this.add.image(0, 0, 'background').setOrigin(0, 0);
-        this.add.text(50, 50, this.text, {fontFamily: 'pixellari', fontSize: '20px', color: '#111155', stroke: '#fff', strokeThickness: 1 });
+        this.add.text(50, 50, this.text, { fontFamily: 'pixellari', fontSize: '20px', color: '#111155', stroke: '#fff', strokeThickness: 1 });
         this.back = new Button(this, 185, 500, 'back', onBack);
         this.back.scaleX = 0.5;
         this.back.scaleY = 0.5;
