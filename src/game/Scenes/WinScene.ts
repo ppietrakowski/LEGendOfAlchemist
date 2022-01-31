807936487f6d7cc10f9d75ab927a3a5fb26a2710
pawelp <80946292+ppietrakowski@users.noreply.github.com>
2022-01-31 23:31:18 +0100

    fixed bug

--- src/game/Scenes/WinScene.ts
+++ src/game/Scenes/WinScene.ts
@@ -2,7 +2,7 @@
 import Button from "../Entities/Button";
 import MainMenu from "./MainMenu";
 function onButtonPressed(this: Phaser.GameObjects.Sprite): void {
-    this.scene.game.scene.switch('DeadScene', 'MainMenu');
+    this.scene.game.scene.switch('WinScene', 'MainMenu');
     (this.scene.game.scene.getScene('MainMenu') as MainMenu).theme.play();
 }
 
