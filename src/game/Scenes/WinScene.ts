414fff3f09e5294c8dc5521cf2f993407431d55d
pawelp <80946292+ppietrakowski@users.noreply.github.com>
2022-01-31 23:29:46 +0100

    changed implementation of WinScene

--- src/game/Scenes/WinScene.ts
+++ src/game/Scenes/WinScene.ts
@@ -1,19 +1,23 @@
 
+import Button from "../Entities/Button";
+import MainMenu from "./MainMenu";
+function onButtonPressed(this: Phaser.GameObjects.Sprite): void {
+    this.scene.game.scene.switch('DeadScene', 'MainMenu');
+    (this.scene.game.scene.getScene('MainMenu') as MainMenu).theme.play();
+}
+
 
 export default class WinScene extends Phaser.Scene {
 
+    back: Button;
+
     constructor() {
-        super('WinScene')
+        super('WinScene');
     }
 
     create(): void {
-
-        this.time.addEvent({
-            elapsed: 1000,
-            callback: () => this.game.scene.switch('WinScene', 'MainMenu')
-    })
-        
+        this.add.image(0, 0, 'background').setOrigin(0, 0).setTint(0xffff00);
+        this.add.text(this.scale.width / 2, this.scale.height / 2, 'You defeated the boss!', {fontSize: '40px', fontFamily: 'pixellari', color: '#ff1155', stroke: '#fff', strokeThickness: 1}).setOrigin(0.5);
+        this.back = new Button(this, this.scale.width / 2, this.scale.height - 120, 'back', onButtonPressed);
     }
-
-
 }
