09139c49eb353a22cea8a3db27708a8ce0320896
pawelp <pawelpietrakowski22012001@gmail.com>
2022-04-12 00:08:28 +0200

    Added todo comment in EnemyController

--- src/game/Components/Effect.ts
+++ src/game/Components/Effect.ts
@@ -3,39 +3,39 @@ import Component from './Component';
 
 
 export default class Effect implements Component {
-    deltaHp: number;
-    deltaStrength: number;
-    deltaWisdom: number;
-    character: Character;
-    private timePassed: number;
-    readonly duration: number;
+    deltaHp: number
+    deltaStrength: number
+    deltaWisdom: number
+    character: Character
+    private timePassed: number
+    readonly duration: number
 
     constructor(deltaHp: number, deltaStrength: number, deltaWisdom: number, duration: number) {
-        this.deltaHp = deltaHp;
-        this.deltaStrength = deltaStrength;
-        this.deltaWisdom = deltaWisdom;
-        this.timePassed = 0;
-        this.duration = duration;
+        this.deltaHp = deltaHp
+        this.deltaStrength = deltaStrength
+        this.deltaWisdom = deltaWisdom
+        this.timePassed = 0
+        this.duration = duration
     }
 
     getName(): string {
-        return 'Effect';
+        return 'Effect'
     }
 
     start(character: Character): void {
-        this.character = character;
+        this.character = character
     }
 
     update(timeSinceLastFrame: number): void {
-        this.timePassed += timeSinceLastFrame;
-        let atrributes = this.character.attributes;
+        this.timePassed += timeSinceLastFrame
+        let atrributes = this.character.attributes
 
-        atrributes.hp += this.deltaHp * timeSinceLastFrame;
-        atrributes.wisdom += this.deltaWisdom * timeSinceLastFrame;
-        atrributes.strength += this.deltaStrength * timeSinceLastFrame;
+        atrributes.hp += this.deltaHp * timeSinceLastFrame
+        atrributes.wisdom += this.deltaWisdom * timeSinceLastFrame
+        atrributes.strength += this.deltaStrength * timeSinceLastFrame
     }
 
-    hasTimePassed(): boolean {
+    public hasTimePassed(): boolean {
         return this.timePassed >= this.duration;
     }
 }
--- src/game/Components/EnemyController.ts
+++ src/game/Components/EnemyController.ts
@@ -25,6 +25,9 @@ function getRoamingPosition(startPos: Phaser.Math.Vector2): Phaser.Math.Vector2 
     return new Phaser.Math.Vector2(startPos.x + v.x * range, startPos.y + v.y * range);
 }
 
+/**
+ * TODO split behaviours to separate classes - pawelp
+ */
 export default class EnemyController implements Component {
     target: Player;
     self: Enemy;
@@ -34,9 +37,9 @@ export default class EnemyController implements Component {
     hitSound: Phaser.Sound.BaseSound;
 
     constructor(target: Player, maxRange: number) {
-        this.target = target;
-        this.state = AI_State.Roaming;
-        this.maxRange = maxRange;
+        this.target = target
+        this.state = AI_State.Roaming
+        this.maxRange = maxRange
     }
 
     getName(): string {
@@ -44,140 +47,139 @@ export default class EnemyController implements Component {
     }
 
     start(character: Character): void {
-        this.self = character as Enemy;
-        console.log(this.self)
+        this.self = character as Enemy
 
         this.self.setVelocityX(0)
         this.self.setVelocityY(0)
-        this.endPos = new Phaser.Math.Vector2(character.x, character.y);
-        character.scene.physics.add.collider(character, this.target);
+        this.endPos = new Phaser.Math.Vector2(character.x, character.y)
+        character.scene.physics.add.collider(character, this.target)
 
-        this.hitSound = this.self.scene.sound.add('player-slap');
+        this.hitSound = this.self.scene.sound.add('player-slap')
     }
 
     update(timeSinceLastFrame: number): void {
-        this.state = this.getNextState();
+        this.state = this.getNextState()
 
         if (this.state === AI_State.DuringMove)
             this.onMoving(timeSinceLastFrame)
         if (this.state === AI_State.Roaming)
-            this.onRoam(timeSinceLastFrame);
+            this.onRoam(timeSinceLastFrame)
         else if (this.state === AI_State.Chasing)
-            this.onChase(timeSinceLastFrame);
+            this.onChase(timeSinceLastFrame)
         else if (this.state === AI_State.Attack)
-            this.onAttack(timeSinceLastFrame);
+            this.onAttack(timeSinceLastFrame)
         else if (this.state === AI_State.Aborted)
-            this.onAbort(timeSinceLastFrame);
+            this.onAbort(timeSinceLastFrame)
     }
 
     private getNextState(): AI_State {
-        let state: AI_State;
+        let state: AI_State
 
         if (this.isPlayerNear())
-            state = AI_State.Attack;
+            state = AI_State.Attack
         else if (this.isPlayerInRange())
-            state = AI_State.Chasing;
+            state = AI_State.Chasing
         else if (this.isPlayerOutOfRange() && this.state === AI_State.Chasing)
-            state = AI_State.Aborted;
+            state = AI_State.Aborted
         else if (this.state === AI_State.DuringMove)
-            state = AI_State.DuringMove;
+            state = AI_State.DuringMove
         else
-            state = AI_State.Roaming;
+            state = AI_State.Roaming
 
-        return state;
+        return state
     }
 
     private getDamage(timeSinceLastFrame: number): number {
-        return -timeSinceLastFrame * 8 * this.self.attributes.strength;
+        return -timeSinceLastFrame * 8 * this.self.attributes.strength
     }
 
     private isPlayerNear(): boolean {
-        return this.self.isNearObject(this.target, 67);
+        return this.self.isNearObject(this.target, 67)
     }
 
     private isPlayerInRange(): boolean {
-        return this.self.isNearObject(this.target, this.maxRange);
+        return this.self.isNearObject(this.target, this.maxRange)
     }
 
     private isPlayerOutOfRange(): boolean {
-        return this.self.isNearObject(this.target, this.maxRange + 100);
+        return this.self.isNearObject(this.target, this.maxRange + 100)
     }
 
     private switchToRoaming(): void {
-        this.self.setVelocity(0, 0);
-        this.state = AI_State.Roaming;
+        this.self.setVelocity(0, 0)
+        this.state = AI_State.Roaming
     }
 
     private onMoveUpOrDown(vel: Phaser.Math.Vector2): void {
         if (vel.y < 0)
-            this.self.anims.play(`${this.self.name}-back-run`, true);
+            this.self.anims.play(`${this.self.name}-back-run`, true)
         else
-            this.self.anims.play(`${this.self.name}-front-run`, true);
+            this.self.anims.play(`${this.self.name}-front-run`, true)
     }
 
     private onDirected(condition: boolean, vel: Phaser.Math.Vector2, animName: string): void {
         // left
         if (condition)
-            this.self.anims.play(animName, true);
+            this.self.anims.play(animName, true)
         else
-            this.onMoveUpOrDown(vel);
+            this.onMoveUpOrDown(vel)
     }
 
     private isDirectedInRightSide(): boolean {
-        return this.self.body.velocity.x > 0;
+        return this.self.body.velocity.x > 0
     }
 
     private playMoveAnim(): void {
-        let vel = this.self.body.velocity;
+        let vel = this.self.body.velocity
 
         if (this.isDirectedInRightSide())
-            this.onDirected(vel.x > Math.abs(vel.y), vel, `${this.self.name}-right-run`);
+            this.onDirected(vel.x > Math.abs(vel.y), vel, `${this.self.name}-right-run`)
         else
-            this.onDirected(Math.abs(vel.x) > Math.abs(vel.y), vel, `${this.self.name}-left-run`);
+            this.onDirected(Math.abs(vel.x) > Math.abs(vel.y), vel, `${this.self.name}-left-run`)
     }
 
     // TODO proper movement
     private onRoam(timeSinceLastFrame: number): void {
-        this.self.setVelocity(0, 0);
-        this.endPos = getRoamingPosition(this.endPos);
+        this.self.setVelocity(0, 0)
+        this.endPos = getRoamingPosition(this.endPos)
 
-        this.state = AI_State.DuringMove;
+        this.state = AI_State.DuringMove
 
 
-        this.self.scene.physics.moveTo(this.self, this.endPos.x, this.endPos.y);
-        this.playMoveAnim();
+        this.self.scene.physics.moveTo(this.self, this.endPos.x, this.endPos.y)
+        this.playMoveAnim()
     }
 
     private onAttack(timeSinceLastFrame: number): void {
         // stops after chasing
-        this.self.setVelocity(0, 0);
+        this.self.setVelocity(0, 0)
 
         if (!this.hitSound.isPlaying) {
-            this.hitSound.play();
+            this.hitSound.play()
             // just attack
-            this.target.attributes.addEffect(new Effect(this.getDamage(timeSinceLastFrame), 0, 0, 1));
+            this.target.attributes.addEffect(new Effect(this.getDamage(timeSinceLastFrame), 0, 0, 1))
         }
     }
 
     private onChase(timeSinceLastFrame: number): void {
-        this.self.scene.physics.moveToObject(this.self, this.target, 40);
-        this.playMoveAnim();
+        this.self.scene.physics.moveToObject(this.self, this.target, 40)
+        this.playMoveAnim()
     }
 
     private onAbort(timeSinceLastFrame: number): void {
         // stops, if it chasing
-        this.self.setVelocity(0, 0);
-        this.switchToRoaming();
+        this.self.setVelocity(0, 0)
+        this.switchToRoaming()
     }
 
     private onMoving(timeSinceLastFrame: number): void {
         if (this.self.isNear(this.endPos, 1.5)) {
-            this.switchToRoaming();
+            this.switchToRoaming()
         }
         else if (this.self.body.checkCollision.down || this.self.body.checkCollision.left || this.self.body.checkCollision.right || this.self.body.checkCollision.up) {
-            this.self.body.velocity.x *= -1;
-            this.self.body.velocity.y *= -1;
-            this.switchToRoaming();
+            this.self.body.velocity.x *= -1
+            this.self.body.velocity.y *= -1
+            this.switchToRoaming()
         }
     }
 }
